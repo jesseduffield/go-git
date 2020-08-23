ca089de7fc234363ef71b9fd9627864bf729b5d4
Jesse Duffield <jessedduffield@gmail.com>
2020-08-23 16:27:49 +1000

    ignore branch unmarhsalling errors

--- config/config.go
+++ config/config.go
@@ -290,9 +290,9 @@ func (c *Config) Unmarshal(b []byte) error {
 	}
 	unmarshalSubmodules(c.Raw, c.Submodules)
 
-	if err := c.unmarshalBranches(); err != nil {
-		return err
-	}
+	// ignore error
+	// Why ignore the error? It seems overly strict and for my use case none of the errors matter to me
+	c.unmarshalBranches()
 
 	if err := c.unmarshalURLs(); err != nil {
 		return err
@@ -393,7 +393,7 @@ func (c *Config) unmarshalBranches() error {
 		b := &Branch{}
 
 		if err := b.unmarshal(sub); err != nil {
-			return err
+			// ignore error
 		}
 
 		c.Branches[b.Name] = b
